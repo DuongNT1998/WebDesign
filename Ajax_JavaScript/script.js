50ee3331c7429302a049872edcf1ba3663d7a14b
Dương Dương <43409498+DuongNT1998@users.noreply.github.com>
2025-10-23 09:07:21 +0700

    Update script.js

--- Ajax_JavaScript/script.js
+++ Ajax_JavaScript/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const API_URL = "https://68f8e830deff18f212b800cc.mockapi.io/notes";
+  const API_URL = "https://68f97a67ef8b2e621e7c2f1b.mockapi.io/Notes";
 
   // --- GET: Lấy danh sách ghi chú ---
   $("#loadNotes").click(function() {
@@ -8,8 +8,14 @@ $(document).ready(function() {
       type: "GET",
       success: function(data) {
         $("#noteList").empty();
+        $("#noteList").append(`<tr>
+<th>Tên công việc</th>
+<th>Nội dung</th>
+<th>Ngày tạo</th>
+<th>Trạng thái</th>
+    </tr>`);
         data.forEach(note => {
-          $("#noteList").append(`<li><b>${note.title}</b>: ${note.content}</li>`);
+          $("#noteList").append(`<tr><td><b>${note.title}</b></td><td>${note.content}</td><td><b>${note.createdAt}</b></td><td><b>${note.done}</b></td></tr>`);
         });
       },
       error: function() {
